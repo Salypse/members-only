c85e81906fd5ea19bba13d52e7cc2c28b11aa7b7
Brandon <84153483+Salypse@users.noreply.github.com>
2026-07-23 14:40:48 -0500

    Update accessibility quality across entire app

--- public/utils/nav.js
+++ public/utils/nav.js
@@ -2,5 +2,14 @@ const menuOnBtn = document.getElementById("menu-on-btn");
 const navLinks = document.getElementById("nav-links");
 
 menuOnBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
+  const isNavOpen = navLinks.classList.toggle("open");
+
+  menuOnBtn.setAttribute("aria-expanded", isNavOpen);
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 800) {
+    navLinks.classList.remove("open");
+    menuOnBtn.setAttribute("aria-expanded", "false");
+  }
 });
